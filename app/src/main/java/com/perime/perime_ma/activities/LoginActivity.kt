906648e3351b43fd544efec8503c4b23ae393bb0
miagomezch <miagomezch@unal.edu.co>
2020-05-04 15:48:21 -0500

    Cambios Botton Switch

--- app/src/main/java/com/perime/perime_ma/activities/LoginActivity.kt
+++ app/src/main/java/com/perime/perime_ma/activities/LoginActivity.kt
@@ -1,13 +1,30 @@
 package com.perime.perime_ma.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.perime.perime_ma.MapsActivity
 import com.perime.perime_ma.R
+import com.perime.perime_ma.extensions.setAllNavigationBarIntentTransitions
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setAllNavigationBarIntentTransitions({goToActivityMap()},{goToActivityPublication()},{goToActivityPublication()},{goToActivityProfile()})
+
+        val btn_registrarse=findViewById(R.id.btn_registrarse) as Button
+
+        btn_registrarse.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
+
+    /* #############    ALL CONFIGURATION TO INTENTS TRANSITIONS - NAVIGATION BAR   ############# */
+    private fun goToActivityMap() = startActivity(Intent(this, MapsActivity::class.java))
+    private fun goToActivityPublication() = startActivity(Intent(this, PublicationActivity::class.java))
+    private fun goToActivityProfile() = startActivity(Intent(this, LoginActivity::class.java))
 }
